981dd2d6621874628c43c93b364414c0eb136286
netomatiazi <netoomn@gmail.com>
2020-08-12 17:39:33 -0300

    Aula 2

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Primeiro from './componentes/Primeiro'
+// import Primeiro from './componentes/Primeiro'
+import BomDia from './componentes/BomDia'
+
 
 // Colocando elemento na página
 
-ReactDOM.render(<Primeiro/>, document.getElementById('root')) 
+// ReactDOM.render(<Primeiro/>, document.getElementById('root')) 
+ReactDOM.render(<BomDia nome="Ovidio"/>, document.getElementById('root')) 
