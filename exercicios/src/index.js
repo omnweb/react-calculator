db5ac92790be027d594c9bdd204bc516899827ad
netomatiazi <netoomn@gmail.com>
2020-08-12 16:35:51 -0300

    Substituição de arquivos

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,17 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Colocando elemento na página
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<h1>React 2</h1>, document.getElementById('root'))
