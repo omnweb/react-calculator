8f7340c8bf4f49779f12ea1a376c8b9c2948d424
netomatiazi <netoomn@gmail.com>
2020-08-14 16:03:59 -0300

    Using Css Var for make style buttons

---
+++ calculadora/src/components/button.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import './button.css'
+
+export default props => <button className="button">{props.label}</button>
--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import './Calculator.css'
+import Button from '../components/button'
+
 
 // Criando a classe Calculator
 export default class Calculator extends Component {
@@ -10,8 +12,24 @@ export default class Calculator extends Component {
             // Retornando uma expressão
             <div className="calculator">
                 {/* Dentro de jsx a class se torna className, pois class é reservada pela linguagem*/}
+                <Button label="AC"/>                
+                <Button label="/"/>                
+                <Button label="7"/>                
+                <Button label="8"/>                
+                <Button label="9"/>                
+                <Button label="*"/>                
+                <Button label="4"/>                
+                <Button label="5"/>                
+                <Button label="6"/>                
+                <Button label="-"/>                
+                <Button label="1"/>                
+                <Button label="2"/>                
+                <Button label="3"/>                
+                <Button label="+"/>                
+                <Button label="0"/>                
+                <Button label="."/>                
+                <Button label="="/>                
                 
-
             </div>
         )
     }
