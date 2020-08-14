2b4a7c740429b3f121636f3da9897e8ef673c59a
netomatiazi <netoomn@gmail.com>
2020-08-14 15:22:39 -0300

    Update Documnents Calculator project

---
+++ calculadora/src/main/Calculator.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import './Calculator.css'
+
+// Criando a classe Calculator
+export default class Calculator extends Component {
+    // Criando a função de renderização
+    render() {
+        //Função necessita de um retorno
+        return(
+            // Retornando uma expressão
+            <div className="calculator">
+                {/* Dentro de jsx a class se torna className, pois class é reservada pela linguagem*/}
+                
+
+            </div>
+        )
+    }
+}
