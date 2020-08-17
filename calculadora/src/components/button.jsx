a2df2277ea80905f1f6435493faf11282289422a
netomatiazi <netoomn@gmail.com>
2020-08-17 10:13:48 -0300

    Building the project events

--- calculadora/src/components/button.jsx
+++ calculadora/src/components/button.jsx
@@ -1,4 +1,18 @@
 import React from 'react'
 import './Button.css'
 
-export default props => <button className="button">{props.label}</button>
+export default props => {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+    return (
+        // A classe button estará sempre presente
+        // Nas demais, caso a propriedade esteja presente o botão recebe a classe, caso contrário não recebe nada
+          <button 
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>
+    )
+}
--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -6,7 +6,32 @@ import Display from '../components/Display'
 
 // Criando a classe Calculator
 export default class Calculator extends Component {
-    // Criando a função de renderização
+    //Reselvendo o problema do this dentro da função arrow usando bind
+
+    constructor (props) {
+        super(props)
+        this.clearMemory = this.clearMemory.bind(this)
+        this.setOperation = this.setOperation.bind(this)
+        this.addDigit = this.addDigit.bind(this)
+    }
+
+    // Função para zerar a calculadora
+    clearMemory(){
+        console.log('limpar');
+    }
+
+    // Função para inserir a operação Matemáica
+    setOperation(operation){
+        console.log(operation);
+    }
+
+    // Adicionar dígito
+
+    addDigit(n){
+        console.log(n);
+    }
+
+    // Função que denderiza a calculadora
     render() {
         //Função necessita de um retorno
         return(
@@ -14,23 +39,23 @@ export default class Calculator extends Component {
             <div className="calculator">
                 {/* Dentro de jsx a class se torna className, pois class é reservada pela linguagem*/}
                 <Display value="100"/>
-                <Button label="AC"/>                
-                <Button label="/"/>                
-                <Button label="7"/>                
-                <Button label="8"/>                
-                <Button label="9"/>                
-                <Button label="*"/>                
-                <Button label="4"/>                
-                <Button label="5"/>                
-                <Button label="6"/>                
-                <Button label="-"/>                
-                <Button label="1"/>                
-                <Button label="2"/>                
-                <Button label="3"/>                
-                <Button label="+"/>                
-                <Button label="0"/>                
-                <Button label="."/>                
-                <Button label="="/>                
+                <Button label="AC" click={this.clearMemory} triple/>                
+                <Button label="/" click ={this.setOperation} operation/>                
+                <Button label="7" click ={this.addDigit}/>               
+                <Button label="8" click ={this.addDigit}/>                
+                <Button label="9" click ={this.addDigit}/>                
+                <Button label="*" click ={this.setOperation} operation/>          
+                <Button label="4" click ={this.addDigit}/>               
+                <Button label="5" click ={this.addDigit}/>                
+                <Button label="6" click ={this.addDigit}/>               
+                <Button label="-" click ={this.setOperation} operation/>              
+                <Button label="1" click ={this.addDigit}/>                
+                <Button label="2" click ={this.addDigit}/>                
+                <Button label="3" click ={this.addDigit}/>                
+                <Button label="+" click ={this.setOperation} operation/>              
+                <Button label="0" click ={this.addDigit} double/> 
+                <Button label="." click ={this.addDigit} />   
+                <Button label="=" click ={this.setOperation} operation/>            
                 
             </div>
         )
