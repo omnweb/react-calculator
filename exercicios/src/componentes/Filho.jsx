33874763cc6717836eba2c169fa497cc48f8070e
netomatiazi <netoomn@gmail.com>
2020-10-20 17:15:35 -0300

    Delete unnecessary files

--- exercicios/src/componentes/Filho.jsx
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-
-export default props => <li>{props.nome} {props.sobrenome}</li>
--- exercicios/src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export function childrenWithProps(props) {
-    return React.Children.map(props.children, child => {
-        return React.cloneElement(child, { 
-            ...props, ...child.props
-        })
-    })
-}
